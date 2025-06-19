4f73bd03f9f26be865221bccb444f461cb301937
andreykarganov <karganov.an@yandex.ru>
2025-06-19 18:05:43 +0300

    1

--- reports/report_generator.py
+++ reports/report_generator.py
@@ -28,7 +28,6 @@ def generate_visits_report(report_date=None, root=None):
             default_filename = f"Отчет_по_посещениям_{date_str}.xlsx"
             report_path = os.path.join(REPORTS_FOLDER, default_filename)
 
-            # Ensure REPORTS_FOLDER exists
             if not os.path.exists(REPORTS_FOLDER):
                 os.makedirs(REPORTS_FOLDER)
 
@@ -96,7 +95,6 @@ def generate_visits_report_everyday(root):
             default_filename = f"Отчет_по_посещениям_РГ_{current_date}.xlsx"
             report_path = os.path.join(REPORTS_FOLDER, default_filename)
 
-            # Ensure REPORTS_FOLDER exists
             if not os.path.exists(REPORTS_FOLDER):
                 os.makedirs(REPORTS_FOLDER)
 
